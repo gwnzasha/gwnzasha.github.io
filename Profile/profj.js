0f46851f5cbcb47b41028d8ee8c2d90af090b208
gwnzasha <gcalago_230000000553@uic.edu.ph>
2024-08-19 01:28:07 +0800

    Delete Profile directory

--- Profile/profj.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('theme-toggle').addEventListener('click', () => {
-    const currentTheme = document.documentElement.getAttribute('data-theme');
-    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-    document.documentElement.setAttribute('data-theme', newTheme);
-    document.getElementById('theme-toggle').classList.toggle('dark-mode', newTheme === 'dark');
-    document.getElementById('theme-toggle').classList.toggle('light-mode', newTheme === 'light');
-});
