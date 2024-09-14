8a61f250b734df540d7d6678a2c6738987101709
gwnzasha <gcalago_230000000553@uic.edu.ph>
2024-09-15 00:01:30 +0800

    Delete Main directory

--- Main/main.js
+++
@@ -1,9 +0,0 @@
-var typed = new Typed('.typing-text span', {
-    strings: ["Web Developer", "Software Developer", "Sleeper", "Cutie", "Asap rocky"],
-    typeSpeed: 100,
-    backSpeed: 50,
-    backDelay: 2000,
-    loop: true
-});
-
-
